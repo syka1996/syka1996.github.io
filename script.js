2ae9c204dcedb5daf92922a4efac6b09fc9f6f99
syka1996 <syuuta1208@yahoo.co.jp>
2025-01-16 19:08:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,23 @@ function getNow() {
     var s = year + "年" + mon + "月" + day + "日" + hour + "時" + min + "分" + sec + "秒";
     return s;
 }
+function osake() {
+    document.getElementById("change_text").textContent = "スーパードライ";
+    document.getElementById("change_text").style.color = "black";
+}
+
+function jusu() {
+    document.getElementById("change_text").textContent = "ジンジャーエール";
+    document.getElementById("change_text").style.color = "black";
+}
+
+function cocktail() {
+    const greetings = ['案山子', 'カルアミルク', 'ジンフィズ', 'シャンディーガフ', 'モスコミュール'];
+    const randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
+    document.getElementById('greeting_text').textContent = randomGreeting;
+}
+
+function ans() {
+    let text = document.getElementById(e.target.id);
+    text.getAttribute("ans") =="True"
+}
